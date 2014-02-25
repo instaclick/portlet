3137ed5d944bff5e4b556d2a075144229d9cc9cb
Helder Santana <helder.css@gmail.com>
2014-02-25 18:19:44 -0500

    send lcov through stdout

--- test/coverage/results.js
+++ test/coverage/results.js
@@ -1,28 +1,11 @@
 var page = require('webpage').create();
 var url  = 'http://localhost:3637/test/specRunner.html#lcov';
-var fs   = require('fs');
 
-// for debug (to see if page returns status code 200)
-page.onResourceReceived = function(response) {
-    if (response.url === url) {
-        console.log('Resorce: "' + response.url + '" status: '  + response.status);
-
-        if (response.status === 200) {
-            console.log(response.url);
-            for (var i = 0; i < response.headers.length; i++) {
-                console.log(response.headers[i].name + ': ' + response.headers[i].value);
-            }
-        }
-    }
-};
-
-page.onConsoleMessage = function (msg) {
-    console.log(msg);
-};
+// page.onConsoleMessage = function (msg) {
+//     console.log(msg);
+// };
 
 page.onLoadFinished = function(status){
-    console.log('Status: ' + status);
-
     page.evaluateAsync(function () {
         document.addEventListener('coverage.ready', function () {
             window.callPhantom(lcov);
@@ -32,7 +15,7 @@ page.onLoadFinished = function(status){
 
 page.onCallback = function (data) {
     try {
-        fs.write('test/coverage/result.lcov', data);
+        console.log(data);
     } catch(e) {
         console.log(e);
     }
