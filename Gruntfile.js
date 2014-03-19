bc16afd24ad0f78796ff69105c8a1669a997153c
Helder Santana <helder.css@gmail.com>
2014-03-19 00:38:18 -0400

    fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,49 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    "use strict";
 
-  var config = {
-    jshint: {
-      all: ['src/**/*.js', 'vendor/bisna/**/*.js'],
-      options: {
-        jshintrc: true
-      }
-    },
-    requirejs: {
-      compile: {
-        options: {
-          baseUrl: "./",
-          name: "portlet",
-          out: "dist/portlet.min.js",
-          paths: {
-            jquery: 'node_modules/jquery/dist/jquery',
-            'jquery.cloneEvent': 'node_modules/jquery.cloneEvent/jquery.cloneEvent',
-            Portlet: 'src',
-            Bisna: 'vendor/bisna'
-          },
-          exclude: ['jquery'],
-          optimize: 'uglify'
+    var config = {
+        jshint: {
+            all: ['Gruntfile.js', 'src/**/*.js', 'vendor/bisna/**/*.js'],
+            options: {
+                jshintrc: true
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: "./",
+                    name: "portlet",
+                    out: "dist/portlet.min.js",
+                    paths: {
+                        jquery: 'node_modules/jquery/dist/jquery',
+                        'jquery.cloneEvent': 'node_modules/jquery.cloneEvent/jquery.cloneEvent',
+                        Portlet: 'src',
+                        Bisna: 'vendor/bisna'
+                    },
+                    exclude: ['jquery'],
+                    optimize: 'uglify'
+                }
+            }
+        },
+        release: {
+            options: {
+                push: false, //default: true
+                pushTags: false, //default: true
+                npm: false, //default: true
+                npmtag: true, //default: no tag
+                tagName: '<%= version %>',
+                commitMessage: 'bump version <%= version %>', //default: 'release <%= version %>'
+                tagMessage: 'tag version <%= version %>'
+            }
         }
-      }
-    }
-  };
+    };
 
-  grunt.initConfig(config);
+    grunt.initConfig(config);
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-release');
 
-  grunt.registerTask('default', ['jshint', 'requirejs']);
+    grunt.registerTask('default', ['jshint', 'requirejs']);
 
 };
